f4c668bee02299c7df1561103295f324ee7a26f8
kiran paturi <kpaturi@smarttuition.com>
2016-05-06 06:46:19 +0530

    UI Routing

--- amazingSales/amazingSales/AngularApp/app/app.js
+++ amazingSales/amazingSales/AngularApp/app/app.js
@@ -10,10 +10,43 @@ angular.module("amazingSales",
 "amazingSales.login",
 "amazingSales.register",
 "amazingSales.cart",
-"amazingSales.gadgets"
+"amazingSales.gadgets",
+"ui.router"
 ]);
 
 angular.module("amazingSales")
-       .config(function () {
-           console.log("i am the Main module");
-       });
+       .config(["$stateProvider", "$urlRouterProvider",
+           function ($stateProvider, $urlRouterProvider) {
+               
+               //Defining States
+               var home = {
+                   name: "home",
+                   url:"/home",
+                   templateUrl:"AngularApp/app/home/home.tpl.html"
+               };
+               var login = {
+                   name: "login",
+                   url: "/login",
+                   templateUrl: "AngularApp/app/login/login.tpl.html"
+               };
+               var register = {
+                   name: "register",
+                   url: "/register",
+                   templateUrl: "AngularApp/app/register/register.tpl.html"
+               };
+               var gadgets = {
+                   name: "gadgets",
+                   url: "/gadgets",
+                   templateUrl: "AngularApp/app/gadgets/gadgets.tpl.html"
+               };
+               
+               //registering the state
+               $stateProvider.state("home", home);
+               $stateProvider.state("login", login);
+               $stateProvider.state("register", register);
+               $stateProvider.state("gadgets", gadgets);
+
+               //if the url don't match then go the default page.
+               $urlRouterProvider.otherwise("/login");
+
+       }]);
